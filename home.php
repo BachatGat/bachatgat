f02933601e44883371dc120b72f0aa6f3cfe542b
sandeepraskar <sandeepraskar@gmail.com>
2017-03-25 11:59:33 +0530

    Update home.php

--- home.php
+++ home.php
@@ -7,8 +7,15 @@ include('session.php');
 <title>*Janseva Parivar*</title>
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="/vendor/formvalidation/dist/js/formValidation.min.js"></script>
+	<script src="/vendor/formvalidation/dist/js/framework/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/account-opening-form.css"/>
+
 </head>
-<body >
+<body>
 
 <div id="wrap">
   <div id="masthead">
@@ -21,26 +28,20 @@ include('session.php');
       <div id="menunav">
         <ul>
           <li><a href="home.php" class="current" ><span>Home</span></a></li>
-          <li><a href="viewmemberdetails.php"><span>View Bachat Gat</span></a></li>
-          <li><a href="openUpdate.html" ><span>Create Member</span></a></li>
-          <li><a href="#" ><span>View Members</span></a></li>
+          <li><a href="bachatgatinfo.html"><span>View Bachat Gat</span></a></li>
+          <li><a href="openUpdate.php" ><span>Create Member</span></a></li>
+          <li><a href="viewmemberdetails.php" ><span>View Members</span></a></li>
           <li><a href="payment1.php" ><span>Payments</span></a></li>
-          <li><a href="#"><span>About Us</span></a></li>
-          <li><a href="#"><span>Profile</span></a></li>
+          <li><a href="viewprofile.html"><span>Profile</span></a></li>
         </ul>
       </div>
     </div>
   </div>
  
 </div>
-     
-<div id="content" >
-  <h2><span style="font-weight:bold; color:red;">Welcome to Janseva Paraivar,Mumbai</span></h2>
+    <br> <br>
   
-
+<div id="footer"> <a href="home.php">homepage</a> | <a href="mailto:ksuresh005@gmail.com">contact</a> | <a href="http://validator.w3.org/check?uri=referer">html</a> 
 </div>
-  
-<div id="footer"> <a href="Link">homepage</a> | <a href="mailto:ksuresh005@gmail.com">contact</a> | <a href="http://validator.w3.org/check?uri=referer">html</a> 
-</div>
-
+</body>
 </html>
